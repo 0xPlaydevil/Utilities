1ca7aa596c30fdcb7e6d6ce05f46d4d91fbb9165
Rick <Rick@163.com>
2020-12-01 17:20:48 +0800

    添加Config

---
+++ Config/Config.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class Config : MonoBehaviour {
+	public string jsonPath = null;
+	private string jsonStr = null;
+	private JSONObject jsonRoot = null;
+	public bool Initialized
+	{
+		get
+		{return jsonStr != null;}
+	}
+	private static Config _instance = null;
+	public static Config instance
+	{
+		get
+		{
+			if(_instance==null)
+			{
+				_instance = FindObjectOfType<Config>();
+				if(_instance!=null)
+				{
+					_instance.Init();
+				}
+			}
+			return _instance;
+		}
+	}
+
+	public string ReadJsonFile(string path)
+	{
+		if(File.Exists(path))
+		{
+			return File.ReadAllText(path);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public JSONObject GetJsonObj(string uri)
+	{
+		string[] fields = uri.Split("/".ToCharArray(),StringSplitOptions.RemoveEmptyEntries);
+		JSONObject jo = jsonRoot;
+		foreach(string field in fields)
+		{
+			jo = jo.GetField(field);
+			if(jo == null)
+			{
+				return null;
+			}
+		}
+		return jo;
+	}
+
+	public string GetString(string uri, string fallback)
+	{
+		JSONObject jObj=GetJsonObj(uri);
+		if(jObj!=null && jObj.IsString)
+		{
+			return jObj.str;
+		}
+		return fallback;
+	}
+
+	public float GetFloat(string uri, float fallback)
+	{
+		JSONObject jObj=GetJsonObj(uri);
+		if(jObj!=null && jObj.IsNumber)
+		{
+			return jObj.f;
+		}
+		return fallback;
+	}
+	public int GetInt(string uri, int fallback)
+	{
+		JSONObject jObj=GetJsonObj(uri);
+		if(jObj!=null && jObj.IsNumber)
+		{
+			return (int)jObj.i;
+		}
+		return fallback;
+	}
+	public bool GetBool(string uri, bool fallback)
+	{
+		JSONObject jObj=GetJsonObj(uri);
+		if(jObj!=null && jObj.IsBool)
+		{
+			return jObj.b;
+		}
+		return fallback;
+	}
+	
+	void Init()
+	{
+		jsonStr = ReadJsonFile(jsonPath);
+		jsonRoot = new JSONObject(jsonStr);
+	}
+
+	void OnEnable()
+	{
+		if(_instance==null)
+		{
+			_instance = this;
+			if(!Initialized)
+			{
+				Init();
+			}
+		}
+		else if(_instance!=this)
+		{
+			Debug.LogWarning("Try to initialize another instance for singleton class Config!");
+		}
+	}
+
+	void OnDestroy()
+	{
+		if(_instance==this)
+		{
+			_instance = null;
+		}
+	}
+}
---
+++ Config/Example/ConfigExample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigExample : MonoBehaviour {
+	// Use this for initialization
+	void Awake () {
+		// 配置视频地址
+		JSONObject videoAddr = Config.instance.GetJsonObj("VideoAddr");
+		if(videoAddr!= null)
+		{
+			string text = videoAddr.str;
+			print(text);
+		}
+	}
+
+	void Start()
+	{
+		JSONObject robotShow = Config.instance.GetJsonObj("RobotShow");
+		if(robotShow!=null && robotShow.b)
+		{
+			print("RobotShow exist.");
+		}
+	}
+
+}
